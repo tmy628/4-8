d423d7f53e75f1501bc8d599d699ab6a954fe33d
tmy628 <tmy6h1r@gmail.com>
2021-03-17 23:21:20 +0000

    [Fix] 4-8

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
-  get 'homes/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  resources :books
 end
