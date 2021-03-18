8e1a97190637eadf2c49ceb1a65cb59588e9776e
tmy628 <tmy6h1r@gmail.com>
2021-03-18 10:44:45 +0000

    [Fix] 4-8

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def index
-  end
+ def index
+   @books = Book.all
+   @book = Book.new
+ end
 
-  def show
-  end
+ def create
+  #1.データを新規登録するためのインスタンス作成
+  book = Book.new(book_params)
+  #2. データをデータベースに保存するためのsaveメソッド実行
+  book.save
+  #3.Books画面へリダイレクト → edirect_to '/books'を削除 → 詳細画面へリダイレクト
+  redirect_to book_path(@book.id)
+ end
 
-  def new
-  end
+ def show
+  @book = Book.find(params[:id])
+ end
 
-  def edit
-  end
+ def new
+ end
+
+ def edit
+  @book = Book.find(params[:id])
+ end
+
+ def updete
+  book = Book.find(params[:id])
+  book.update(book_params)
+  redirect_to book_path(book.id)
+ end
+
+ def destroy
+  book = Book.find(params[:id]) #データ(レコード)を1件取得
+  book.destroy #データ（レコード）を削除r
+  redirect_to books_path
+ end
+
+ private
+  #ストロングパラメータ
+ def book_params
+  params.require(:book).permit(:title, :body)
+ end
 end
--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'homes/top'
-  resources :books
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'books' => 'books#index', as: 'index_book'
+  post 'books' => 'books#create'
+
+  # .../books/1 や .../books/3 に該当する
+  get 'books/:id' => 'books#show'
+  get 'books/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'book/:id' => 'books#update', as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
